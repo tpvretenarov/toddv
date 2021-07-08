d3d485ecf46ee7f4d2b63712d803f59ecf186ce3
Todor Vretenarov <vretenarovtodor@gmail.com>
2021-07-08 19:38:11 -0400

    feat: added react

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="d-flex w-100 justify-content-between">
+            <div>ToddV</div>
+            <div>Portfolio</div>
+            <div>Projects</div>
+            <div>Contact</div>
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+    return (
+        <div className="w-100 h-100">
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home;
