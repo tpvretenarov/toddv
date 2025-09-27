ad28710031592d7236534d238eb37ffbcdc77c67
Todor Vretenarov <vretenarovtodor@gmail.com>
2025-09-26 23:34:30 -0400

    new background image, removed scroll bars

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <SEO />
-      <div className="container mx-auto flex h-full flex-col overflow-scroll p-8 text-white lg:flex-row lg:p-0">
+      <div className="container mx-auto flex h-full flex-col p-8 text-white antialiased lg:flex-row lg:p-0">
         <Sidebar />
         <div className="w-full lg:w-1/2 lg:pl-8 lg:pr-24">
           <About />
--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,8 +1,8 @@
 const Pill = ({ label }: { label: string }) => {
   return (
-    <span className="rounded-full bg-[#64ffda]/20 px-[12px] py-[5px] font-mono text-xs text-[#64ffda]">
+    <div className="flex h-6 items-center rounded-full bg-[#64ffda]/20 px-[10px] pt-[1px] font-mono text-xs text-[#64ffda]">
       {label}
-    </span>
+    </div>
   );
 };
 
